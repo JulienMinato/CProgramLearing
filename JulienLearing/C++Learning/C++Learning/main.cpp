9c15e90185b020c3551d4b1c985d749d54159eac
JulienMinato <SulaFelinae@gmail.com>
2017-03-05 19:46:58 +0800

    C++ is coming

---
+++ JulienLearing/C++Learning/C++Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Learning
+//
+//  Created by 赵亮 on 2017/3/5.
+//  Copyright © 2017年 赵亮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
